534e6464054839e5b3d74aa31f97640c5f00cdf6
Trần Danh Doanh <nhimcaptin@gmail.com>
2025-04-22 11:19:43 +0700

    update get menu

--- app/controllers/form_face/index.js
+++ app/controllers/form_face/index.js
@@ -25,7 +25,67 @@ const updateFormFace = async (req, res) => {
   }
 };
 
+const getMenuItems = async (req, res) => {
+  try {
+    const { campaign_id } = req.query;
+
+    if (!campaign_id) {
+      return returnResponse(res, 400, { error: "Business ID is required" });
+    }
+
+    db.query(
+      "SELECT id, campaign as name, resultsCost, costPerResultCost, amountSpent FROM campaigns WHERE id = ?",
+      [campaign_id],
+      async (err, campaignResults) => {
+        if (err) return returnResponse(res, 400, { err });
+
+        const campaigns = await Promise.all(
+          campaignResults.map(async (campaign) => {
+            return new Promise((resolve, reject) => {
+              db.query(
+                "SELECT id, adSet as name FROM ad_sets WHERE campaign_id = ?",
+                [campaign.id],
+                async (err, adSetResults) => {
+                  if (err) reject(err);
+                  const adSets = await Promise.all(
+                    adSetResults.map(async (adSet) => {
+                      return new Promise((resolve, reject) => {
+                        db.query("SELECT id, ad as name FROM ads WHERE ad_set_id = ?", [adSet.id], (err, adResults) => {
+                          if (err) reject(err);
+                          resolve({
+                            id: adSet.id,
+                            name: adSet.name,
+                            groups: adResults,
+                          });
+                        });
+                      });
+                    })
+                  );
+
+                  resolve({
+                    id: campaign.id,
+                    name: campaign.name,
+                    resultsCost: campaign.resultsCost,
+                    costPerResultCost: campaign.costPerResultCost,
+                    amountSpent: campaign.amountSpent,
+                    groups: adSets,
+                  });
+                }
+              );
+            });
+          })
+        );
+
+        return returnResponse(res, 200, { data: campaigns });
+      }
+    );
+  } catch (error) {
+    return returnResponse(res, 500, { error });
+  }
+};
+
 module.exports = {
   updateFormFace,
   getList,
+  getMenuItems,
 };
--- app/router/form_face.js
+++ app/router/form_face.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { updateFormFace, getList } = require("../controllers/form_face/index.js");
+const { updateFormFace, getList, getMenuItems } = require("../controllers/form_face/index.js");
 
 const router = express.Router();
 
 router.put("/update", updateFormFace);
 router.get("/get", getList);
+router.get("/get-menu", getMenuItems);
 
 module.exports = router;
